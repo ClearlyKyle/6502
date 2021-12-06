6cad05dd9cb3a6f157845aa2cc3b9dfd535c2157
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2021-12-06 22:01:19 +0000

    added 3 unit tests

--- src/main.c
+++ src/main.c
@@ -5,18 +5,6 @@
 
 int main(int argc, char const *argv[])
 {
-
-    reset_cpu(&cpu, &mem);
-
-    // start - little program
-    mem.data[0xFFFC] = INS_JSR;
-    mem.data[0xFFFD] = 0x42;
-    mem.data[0xFFFE] = 0x42;
-    mem.data[0x4242] = INS_LDA_IM;
-    mem.data[0x4243] = 0x84;
-    // end - little program
-    execute(9, &mem);
-
     printf("Program Exit...\n");
     return 0;
 }
--- tests/tests.c
+++ tests/tests.c
@@ -3,23 +3,57 @@
 
 // https://github.com/ThrowTheSwitch/Unity
 
-void setUp(void) {}    /* Is run before every test, put unit init calls here. */
+void setUp(void) /* Is run before every test, put unit init calls here. */
+{
+    reset_cpu(&cpu, &mem);
+}
 void tearDown(void) {} /* Is run after every test, put unit clean-up calls here. */
 
-void test_AverageThreeBytes_should_AverageMidRangeValues(void)
+void RunLittleProgram(void)
 {
-    TEST_ASSERT_EQUAL_HEX8(40, 40);
+    // start - little program
+    mem.data[0xFFFC] = INS_JSR;
+    mem.data[0xFFFD] = 0x42;
+    mem.data[0xFFFE] = 0x42;
+    mem.data[0x4242] = INS_LDA_IM;
+    mem.data[0x4243] = 0x84;
+    // end - little program
+
+    execute(9, &mem);
+
+    // TEST_ASSERT_EQUAL(expected, actual)
+    TEST_ASSERT_EQUAL(0x84, cpu.accumulator);
+    TEST_ASSERT_EQUAL_HEX8(0x84, cpu.accumulator);
 }
 
-void test_AverageThreeBytes_should_AverageHighValues(void)
+void LDA_Immediate_Can_Load_A_Value_Into_A_Register(void)
 {
-    TEST_ASSERT_EQUAL_HEX8(80, 80);
+    mem.data[0xFFFC] = INS_LDA_IM;
+    mem.data[0xFFFD] = 0x84;
+
+    execute(2, &mem);
+
+    // TEST_ASSERT_EQUAL(expected, actual)
+    TEST_ASSERT_EQUAL_HEX8(0x84, cpu.accumulator);
+}
+
+void LDAZ_Can_Load_A_Value_Into_A_Register(void)
+{
+    mem.data[0xFFFC] = INS_LDA_ZP;
+    mem.data[0xFFFD] = 0x42;
+    mem.data[0x0042] = 0x37;
+
+    execute(3, &mem);
+
+    // TEST_ASSERT_EQUAL(expected, actual)
+    TEST_ASSERT_EQUAL_HEX8(0x37, cpu.accumulator);
 }
 
 int main(void)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_AverageThreeBytes_should_AverageMidRangeValues);
-    RUN_TEST(test_AverageThreeBytes_should_AverageHighValues);
+    RUN_TEST(RunLittleProgram);
+    RUN_TEST(LDA_Immediate_Can_Load_A_Value_Into_A_Register);
+    RUN_TEST(LDAZ_Can_Load_A_Value_Into_A_Register);
     return UNITY_END();
 }
