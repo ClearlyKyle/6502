d9ecd63a2103985c151e84fcadcb38fe9c42ed35
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-11-02 20:33:48 +0000

    Register_Compare for CMP_ZP

--- src/h6502.h
+++ src/h6502.h
@@ -1526,7 +1526,7 @@ inline s32 Execute(s32 number_of_cycles)
         {
             const u16 address = Address_Zero_Page(&number_of_cycles);
             const u8  operand = Read_Byte(&number_of_cycles, address);
-
+            Register_Compare(operand, cpu.accumulator);
             break;
         }
         case INS_CMP_ZP_X:
