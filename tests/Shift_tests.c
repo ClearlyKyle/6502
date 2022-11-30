0b194953c7ff3199813435b654cf3430916cf601
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-11-30 08:17:01 +0000

    LSR_ZP_Can_Shift_A_Zero_Into_The_Carry_Flag

--- tests/Shift_tests.c
+++ tests/Shift_tests.c
@@ -360,6 +360,32 @@ void LSR_ZP_Can_Shift_The_Value_Of_One(void)
     TEST_ASSERT_TRUE(cpu.Z);
     TEST_ASSERT_FALSE(cpu.N);
 }
+
+void LSR_ZP_Can_Shift_A_Zero_Into_The_Carry_Flag(void)
+{
+    // given:
+    cpu.program_counter = 0xFF00;
+    cpu.C               = 1;
+    cpu.Z               = 1;
+    cpu.N               = 1;
+
+    mem.data[0xFF00] = INS_LSR_ZP;
+    mem.data[0xFF01] = 0x42;
+    mem.data[0x0042] = 8;
+
+    const s32 EXPECTED_CYCLES = 5;
+
+    // when:
+    const s32 actual_cycles = Execute(EXPECTED_CYCLES);
+
+    // then:
+    TEST_ASSERT_EQUAL_INT32(EXPECTED_CYCLES, actual_cycles);
+    TEST_ASSERT_EQUAL(mem.data[0x0042], 4);
+
+    TEST_ASSERT_FALSE(cpu.C);
+    TEST_ASSERT_FALSE(cpu.Z);
+    TEST_ASSERT_FALSE(cpu.N);
+}
 int main(void)
 {
     UNITY_BEGIN();
@@ -380,5 +406,6 @@ int main(void)
     RUN_TEST(LSR_Can_Shift_The_Value_Of_One);
     RUN_TEST(LSR_Can_Shift_A_Zero_Into_The_Carry_Flag);
     RUN_TEST(LSR_ZP_Can_Shift_The_Value_Of_One);
+    RUN_TEST(LSR_ZP_Can_Shift_A_Zero_Into_The_Carry_Flag);
     return UNITY_END();
 }
