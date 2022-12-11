87b1b120ec23713cf5a1862999cf509253f1dec6
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-12-11 19:08:04 +0000

    ROL_ZP_Can_Shift_A_Bit_Out_Of_The_Carry_Flag

--- tests/Shift_tests.c
+++ tests/Shift_tests.c
@@ -652,6 +652,31 @@ void ROL_Can_Shift_A_Value_That_Results_In_A_Negative_Value(void)
     TEST_ASSERT_TRUE(cpu.N);
 }
 
+void ROL_ZP_Can_Shift_A_Bit_Out_Of_The_Carry_Flag(void)
+{
+    // given:
+    cpu.program_counter = 0xFF00;
+    cpu.C               = 1;
+    cpu.Z               = 1;
+    cpu.N               = 1;
+
+    mem.data[0xFF00] = INS_ROL_ZP;
+    mem.data[0xFF01] = 0x42;
+    mem.data[0x0042] = 0;
+
+    const s32 EXPECTED_CYCLES = 5;
+
+    // when:
+    const s32 actual_cycles = Execute(EXPECTED_CYCLES);
+
+    // then:
+    TEST_ASSERT_EQUAL_INT32(EXPECTED_CYCLES, actual_cycles);
+    TEST_ASSERT_EQUAL(mem.data[0x0042], 1);
+
+    TEST_ASSERT_FALSE(cpu.C);
+    TEST_ASSERT_FALSE(cpu.Z);
+    TEST_ASSERT_FALSE(cpu.N);
+}
 int main(void)
 {
     UNITY_BEGIN();
@@ -685,5 +710,6 @@ int main(void)
     RUN_TEST(ROL_Can_Shift_A_Bit_Into_The_Carry_Flag);
     RUN_TEST(ROL_Can_Shift_Zero_With_No_Carry);
     RUN_TEST(ROL_Can_Shift_A_Value_That_Results_In_A_Negative_Value);
+    RUN_TEST(ROL_ZP_Can_Shift_A_Bit_Out_Of_The_Carry_Flag);
     return UNITY_END();
 }
