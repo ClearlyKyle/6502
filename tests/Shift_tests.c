9035fca5ef285a78216eaeae584d8cde17548e2e
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2023-01-01 14:23:38 +0000

    ROR_ZP_Can_Shift_A_Value_Into_The_Carry_Flag

--- tests/Shift_tests.c
+++ tests/Shift_tests.c
@@ -961,6 +961,32 @@ void ROR_ZP_Can_Shift_The_Carry_Flag_Into_The_Operand(void)
     TEST_ASSERT_FALSE(cpu.Z);
     TEST_ASSERT_TRUE(cpu.N);
 }
+
+void ROR_ZP_Can_Shift_A_Value_Into_The_Carry_Flag(void)
+{
+    // given:
+    cpu.program_counter = 0xFF00;
+    cpu.C               = 0;
+    cpu.Z               = 0;
+    cpu.N               = 0;
+
+    mem.data[0xFF00] = INS_ROR_ZP;
+    mem.data[0xFF01] = 0x42;
+    mem.data[0x0042] = 1;
+
+    const s32 EXPECTED_CYCLES = 5;
+
+    // when:
+    const s32 actual_cycles = Execute(EXPECTED_CYCLES);
+
+    // then:
+    TEST_ASSERT_EQUAL_INT32(EXPECTED_CYCLES, actual_cycles);
+    EXPECT_EQ(mem.data[0x0042], 0);
+
+    TEST_ASSERT_TRUE(cpu.C);
+    TEST_ASSERT_TRUE(cpu.Z);
+    TEST_ASSERT_FALSE(cpu.N);
+}
 int main(void)
 {
     UNITY_BEGIN();
@@ -1008,5 +1034,6 @@ int main(void)
     RUN_TESTS(ROR_Can_Shift_A_Value_Into_The_Carry_Flag);
     RUN_TESTS(ROR_Can_Rotate_A_Number);
     RUN_TESTS(ROR_ZP_Can_Shift_The_Carry_Flag_Into_The_Operand);
+    RUN_TESTS(ROR_ZP_Can_Shift_A_Value_Into_The_Carry_Flag);
     return UNITY_END();
 }
